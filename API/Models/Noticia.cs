aa63902d65535bc5ef46b188b959461bf9e9c3c0
Solis <imsolis@hotmail.com>
2020-04-10 12:30:00 -0500

    Creación de la BD y los Models

---
+++ API/Models/Noticia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Noticia{
+        public int NoticiaID { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set;}
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; }
+        public int AutorID { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
